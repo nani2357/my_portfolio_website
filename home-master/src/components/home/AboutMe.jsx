51f28e7a380259431c0ee5b0f2a1c3f5335d719a
naveen_kadampally <naveenkadampally@gmail.com>
2023-08-20 14:43:16 +0100

    bugs_fixed

--- home-master/src/components/home/AboutMe.jsx
+++ home-master/src/components/home/AboutMe.jsx
@@ -36,7 +36,7 @@ const AboutMe = ({ heading, message, link, imgSize, resume }) => {
   return (
     <Jumbotron id="aboutme" className="m-0">
       <div className="container row">
-        <div className="col-5 d-none d-lg-block align-self-center">
+        <div className="col-12 col-md-5 d-flex justify-content-center align-self-center">
           {showPic && (
             <img
               className="border border-secondary rounded-circle"
@@ -47,7 +47,7 @@ const AboutMe = ({ heading, message, link, imgSize, resume }) => {
             />
           )}
         </div>
-        <div className={`col-lg-${showPic ? "7" : "12"}`}>
+        <div className={`col-12 col-md-${showPic ? "7" : "12"}`}>
           <h2 className="display-4 mb-5 text-center">{heading}</h2>
           <p className="lead text-center" dangerouslySetInnerHTML={{ __html: message }}></p>
           {resume && (
